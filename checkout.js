7e7d5a32ca19b7c5e038b1e9cba82f5db1b71944
furkan <furkanesen35@gmail.com>
2022-06-20 00:39:30 +0200

    local storagea and button functions added

--- checkout.js
+++ checkout.js
@@ -8,6 +8,30 @@ window.addEventListener("load", () => {
 });
 
 let productsDiv = document.querySelector(".products");
-productsDiv.addEventListener("click", (e)=> {
- console.log(e.target);
+
+productsDiv.addEventListener("click", (event)=> {
+ if(event.target.className == "minus"){
+  if(event.target.nextElementSibling.innerText > 1){  
+   event.target.nextElementSibling.innerText--;
+  } 
+  else { 
+   if(confirm("Product will be deleted"))
+
+  }
+  // console.log("minus clicked");
+ }
+ else if(event.target.className == "plus"){
+  event.target.previousElementSibling.innerText++;
+  // console.log(event.target.classList);
+  // console.log("plus clicked");
+ }
+ else if(event.target.classList.contains("remove-product") ){
+  event.target.parentElement.parentElement.parentElement.remove();
+  // console.log(event.target.classList);
+  // console.log("remove clicked");
+ }
+ else {
+  // console.log(event.target.classList);
+  // console.log("others clicked"); 
+ }
 })
